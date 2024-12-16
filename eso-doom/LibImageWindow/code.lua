49248ab8b6012e9ad3ef7cc463e9591f4b9daade
akamatsu02 <philipp@aka02.de>
2024-12-16 23:02:16 +0100

    the idea

---
+++ eso-doom/LibImageWindow/code.lua
@@ -0,0 +1,5 @@
+--[[
+idea:
+	a label can have 128 color codes, maybe there is a monospace font with no gaps between the characters and just a black square as character (like: https://www.compart.com/en/unicode/U+25A0)
+	for a screen of 128 x 64 pixels we can put 64 labels below each other and handle the horizontal pixels with the text content of the label (and also the coloring here)
+--]]
